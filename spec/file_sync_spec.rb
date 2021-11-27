8dc5fc548078bcc28bc08de0c3fc65f614ef8433
MustafaErgul <mustafaergul0@gmail.com>
2021-11-27 11:12:22 +0300

    Additional test case

--- spec/file_sync_spec.rb
+++ spec/file_sync_spec.rb
@@ -4,10 +4,18 @@ require_relative '../lib/file_sync'
 
 RSpec.describe FileSync, '#fname' do
   let(:including_class) { FileSync::MyConfig.new { include FileSync } }
-  it 'testing existence of a file' do
+
+  it 'testing existence of a file on the target destination' do
     file_names = ['.vimrc', '.zshrc', '.tmux.conf']
     file_names.flatten.map do |f|
       expect(File).to exist("#{ENV['HOME']}/#{f}")
     end
   end
+
+  it 'testing existence of a file on the source destination' do
+    file_names = ['.vimrc', '.zshrc', '.tmux.conf']
+    file_names.flatten.map do |f|
+      expect(File).to exist("#{ENV['HOME']}/Projects/dotfiles/#{f}")
+  end
+end
 end
