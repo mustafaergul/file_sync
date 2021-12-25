f860a2d7c059f77538831077914ab2289919256a
MustafaErgul <mustafaergul0@gmail.com>
2021-12-25 15:01:13 +0300

    Remove splat operator usage

--- lib/file_sync.rb
+++ lib/file_sync.rb
@@ -14,7 +14,7 @@ module FileSync
   class MyConfig
     attr_accessor :fname
 
-    def initialize(*fname)
+    def initialize(fname)
       @fname = fname
       @dot_dir = %(dotfiles/)
     end
@@ -38,17 +38,15 @@ module FileSync
 
     def sync(file)
       puts "#{file} is syncing..."
-      cmd = "cp ~/#{file} #{ENV['HOME']}/Projects/#{@dot_dir}"
-      exec(cmd)
+      `cp ~/#{file} #{ENV['HOME']}/Projects/#{@dot_dir}`
       puts
     end
   end
 
   files_payload = YAML.load_file('./config/filenames.yml')
 
-  files_payload.values.flatten.map do |v|
-    file = MyConfig.new(v)
-    file.fetch
-    file.compare
-  end
+  files = MyConfig.new(files_payload.values.flatten)
+  files.fetch
+  files.compare
+
 end
